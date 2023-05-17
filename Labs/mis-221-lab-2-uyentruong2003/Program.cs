0d464286ef6bd18ee18ad7a4fbb5da67dc6fc965
uyentruong2003 <121909188+uyentruong2003@users.noreply.github.com>
2023-05-16 20:30:30 -0500

    upload lab2,3

---
+++ Labs/mis-221-lab-2-uyentruong2003/Program.cs
@@ -0,0 +1,41 @@
+﻿// Inputs: ordersFilled, milesDriven, ordersMade
+int ordersFilled;
+int ordersMade;
+double milesDriven;
+// Outputs: fulfillmentRate, costPerOrder
+double fulfillmentRate;
+double costPerOrder;
+// Processing items: DRIVERS_WAGE (constant), milesPerOrder
+const double DRIVERS_WAGE = 1.27;
+double milesPerOrder;
+
+// ****** MAIN *******
+// Prompt user for the orders filled
+Console.WriteLine("Input the number of orders filled: ");
+ordersFilled = int.Parse(Console.ReadLine());
+
+// Prompt users for orders made
+Console.WriteLine("Input the number of orders made: ");
+ordersMade = int.Parse(Console.ReadLine());
+
+// Prompt users for the miles driven
+Console.WriteLine("Input the number of miles driven: ");
+milesDriven = double.Parse(Console.ReadLine());
+
+// Calculate the fulfillment rate:
+fulfillmentRate = ordersFilled / ordersMade;
+
+// Calculate the miles-per-order rate:
+milesPerOrder = ordersFilled / milesDriven;
+    // Hi, I think the formula given in the IPO chart is wrong. 
+    // To calculate the milesPerOrder, I think should use this instead:
+    // milesPerOrder = milesDriven/ordersFilled
+    // I'm still following the formula in the IPO chart FYI.
+
+// Calculate the cost-per-order rate:
+costPerOrder = milesPerOrder * DRIVERS_WAGE;
+
+// Print out fulfillmentRate and costPerOrder:
+Console.WriteLine("Fulfillment Rate: " + fulfillmentRate);
+Console.WriteLine("Cost per Order: $" + costPerOrder);
+
---
+++ Labs/mis221-lab-3-uyentruong2003/Program.cs
@@ -0,0 +1,43 @@
+﻿// Start main
+
+// Prompt user to enter the day of the week
+string dayOfWeek = GetDayOfWeek();
+// Check if the day has a birthday & return the corresponding TA's name:
+string nameTA = BirthdayDecision(dayOfWeek);
+// Display HPBD message for the correct TA:
+SayHappyBirthday(nameTA);
+
+// End main
+
+// method to get day of the week input:
+static string GetDayOfWeek(){
+    System.Console.WriteLine("What day is today (Monday, Tuesday, etc.)? ");
+    return Console.ReadLine();
+}
+
+
+// method to test whose bd it is:
+static string BirthdayDecision(string day){
+    switch(day){
+        case "Tuesday":
+            return "Makayla";
+            break;
+        case "Friday":
+            return "Alex";
+            break;
+        default:
+            return "N/A";
+            break;
+
+    }
+}
+
+
+// method to display HPBD message:
+static void SayHappyBirthday(string name){
+    if (name == "N/A"){
+        System.Console.WriteLine("No birthdays today :(");
+    } else{
+        System.Console.WriteLine($"Happy Birthday, {name}!");
+    }
+}
